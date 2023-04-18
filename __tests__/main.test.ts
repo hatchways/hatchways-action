9317d7a1f59f859a0d8d706a57737aa0c0824f38
Shums Kassam <skassam21@gmail.com>
2023-04-18 13:07:43 -0400

    Add Readme

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,7 +6,7 @@ import {expect, test} from '@jest/globals'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_API_KEY'] = '5'
-  process.env['INPUT_FILES'] = '- report.xml'
+  process.env['INPUT_FILES'] = '- outputs/*.xml'
   process.env['INPUT_API_URL'] = 'http://localhost:8080/api/github/actions'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
